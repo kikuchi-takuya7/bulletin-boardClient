a7377711630458fab136a21a6e352df2a214f3d6
巧椰 菊地 <220065@jc-21.jp>
2024-02-27 10:09:00 +0900

    文字列だるい

--- bulletin boardClient/main.cpp
+++ bulletin boardClient/main.cpp
@@ -16,6 +16,8 @@ const unsigned short SERVERPORT = 8888;
 // 送受信するメッセージの最大値
 const unsigned int MESSAGELENGTH = 1024;
 
+const string IP = "192.168.42.32";
+
 int main()
 {
 
@@ -38,11 +40,56 @@ int main()
     sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
     // 接続先サーバのIPアドレスを入力させる
-    string serverIpAddress;
-    cout << "Input Server IPv4 address :";
-    cin >> serverIpAddress;
-
+    string serverIpAddress = IP;
+    //cout << "Input Server IPv4 address :";
+    //cin >> serverIpAddress;
+    
+    //サーバーからAllOKが来るまでプレイヤーの受付を行う。
+    int playerNum = 0;
     while (true)
+    {
+
+        playerNum++;
+
+        // 接続先サーバのソケットアドレス情報格納
+        SOCKADDR_IN toAddr;
+        const int tolen = sizeof(toAddr);
+        memset(&toAddr, 0, sizeof(toAddr));
+        toAddr.sin_family = AF_INET;
+        toAddr.sin_port = htons(SERVERPORT);
+        inet_pton(AF_INET, serverIpAddress.c_str(), &toAddr.sin_addr.s_addr);
+
+        char message[MESSAGELENGTH];	// サーバ側とサイズを合わせること
+        SOCKADDR_IN fromAddr;
+        int fromlen = sizeof(fromAddr);
+        cout << "ユーザー名の入力:";
+        cin >> message;
+        ret = sendto(sock, message, sizeof(message), 0, (SOCKADDR*)&toAddr, tolen);
+        if (ret == SOCKET_ERROR)
+        {
+            /*
+                エラー処理
+            */
+        }
+        ret = recvfrom(sock, message, sizeof(message), 0, (SOCKADDR*)&fromAddr, &fromlen);
+
+        if (ret == SOCKET_ERROR)
+        {
+
+        }
+        else
+        {
+            std::cout << message << std::endl;
+        }
+
+        char test[MESSAGELENGTH] = "AllOK";
+        if (message == test) {
+            break;
+        }
+
+    }
+
+    for(int i=0;i<playerNum;i++)
     {
         // 接続先サーバのソケットアドレス情報格納
         SOCKADDR_IN toAddr;
@@ -55,10 +102,7 @@ int main()
         char message[MESSAGELENGTH];	// サーバ側とサイズを合わせること
         SOCKADDR_IN fromAddr;
         int fromlen = sizeof(fromAddr);
-        char Hp = '0';
-        char Attack = '0';
-        char Magic = '0';
-        cout << "Input message:";
+        cout << "プレイヤー順に出す手の入力:";
         cin >> message;
         ret = sendto(sock, message, sizeof(message), 0, (SOCKADDR*)&toAddr, tolen);
         if (ret == SOCKET_ERROR)
@@ -67,8 +111,16 @@ int main()
                 エラー処理
             */
         }
+
+    }
+    for (int i = 0; i < playerNum; i++) {
+
+
+        char message[MESSAGELENGTH];	// サーバ側とサイズを合わせること
+        SOCKADDR_IN fromAddr;
+        int fromlen = sizeof(fromAddr);
         ret = recvfrom(sock, message, sizeof(message), 0, (SOCKADDR*)&fromAddr, &fromlen);
-        
+
         if (ret == SOCKET_ERROR)
         {
 
@@ -76,6 +128,10 @@ int main()
         else
         {
             std::cout << message << std::endl;
+        }
+
+        if (message == "AllOK") {
+            break;
         }
     }
 
